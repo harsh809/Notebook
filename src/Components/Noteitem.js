8011b697c11eccf40c83e76f6fc4aaa3b37f6b22
Harsh <hk3711750@gmail.com>
2024-01-17 01:30:31 +0530

    third commit

--- src/Components/Noteitem.js
+++ src/Components/Noteitem.js
@@ -1,18 +1,30 @@
-import React,{ useContext } from 'react'
+import React, { useContext } from 'react'
 import notescontext from '../context/notes/notescontext';
+import DeleteIcon from '@mui/icons-material/Delete';
+import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 
 function Noteitem(props) {
     const context = useContext(notescontext);
     const { deletenote } = context;
-    const { note,update } = props;
+    const { note, update } = props;
     return (
         <div className='col-md-3'>
             <div className="card my-3" >
                 <div className="card-body">
                     <div className="d-flex align-items-center">
-                        <h5 className="card-title mx-2">{note.title}</h5>
-                        <i className="fa-solid fa-trash-can mx-2" onClick={()=>{deletenote(note._id); props.showalert("Successfully Deleted","success");}}></i>
-                        <i className="fa-solid fa-file-pen mx-2" onClick={()=>update(note)}></i>
+                        <h5 className="card-title mx-2 overflow-hidden">{note.title}</h5>
+                        <Tooltip title="Delete" onClick={() => { deletenote(note._id); props.showalert("Successfully Deleted", "success"); }}>
+                            <IconButton>
+                                <DeleteIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Update" onClick={() => update(note)}>
+                            <IconButton>
+                                <DriveFileRenameOutlineIcon onClick={() => update(note)} />
+                            </IconButton>
+                        </Tooltip>
                     </div>
                     <p className="card-text">{note.description}</p>
                 </div>
